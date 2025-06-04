b9eaa32e4022430f518c452ef72ddd00bcb389c1
AmehSmart <amehsunday575@gmail.com>
2025-06-04 11:22:13 +0100

    backend

---
+++ backend/API/account/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('posts/', views.PostListViewCreate.as_view(), name='post-list'),
+    path('posts/delete/<int:pk>/', views.DeletePostView.as_view(), name='post-delete'),
+]
