db3010c5cd3a1c1038b460a35aab08fddce58377
CinnamonfirJ <cinnamonfir@gmail.com>
2025-07-13 13:23:53 +0100

    feat: add CSRF token handling in login route; clean up cookie setting logic

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,17 +1,23 @@
 // app/api/auth/login/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import cookie from "cookie";
+import { parse } from "path";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
+  const rawCookies = req.headers.get("cookie") || "";
+  const parsed = parse(rawCookies);
+  const csrfToken = parsed["csrftoken"];
+
   const res = await fetch(`${API_BASE_URL}/dj-rest-auth/login/`, {
     method: "POST",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
+      "X-CSRFToken": csrfToken,
     },
     body: JSON.stringify(body),
   });
@@ -23,29 +29,6 @@ export async function POST(req: NextRequest) {
   const data = await res.json();
 
   const response = NextResponse.json({ success: true });
-
-  // Set cookies (HttpOnly & Secure)
-  // response.headers.append(
-  //   "Set-Cookie",
-  //   cookie.serialize("access", data.access, {
-  //     httpOnly: true,
-  //     secure: process.env.NODE_ENV === "production",
-  //     sameSite: "strict",
-  //     path: "/",
-  //     maxAge: 60 * 60, // 1 hour
-  //   })
-  // );
-
-  // response.headers.append(
-  //   "Set-Cookie",
-  //   cookie.serialize("refresh", data.refresh, {
-  //     httpOnly: true,
-  //     secure: process.env.NODE_ENV === "production",
-  //     sameSite: "strict",
-  //     path: "/",
-  //     maxAge: 60 * 60 * 24 * 7, // 7 days
-  //   })
-  // );
 
   response.headers.set(
     "Set-Cookie",
